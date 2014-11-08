230e14c1ef85de2a0b4554ded642a74cd8dd13a6
Daniel Weinmann <danielweinmann@gmail.com>
2014-11-08 19:17:58 -0200

    New round of adaptation to UnlockGateway

--- lib/unlock_paypal/models/contribution.rb
+++ lib/unlock_paypal/models/contribution.rb
@@ -4,61 +4,12 @@ module UnlockPaypal
 
       include UnlockGateway::Models::Contribution
 
-      def paypal_auth
-        return {} unless self.gateway && self.gateway.settings
-        { paypal_auth: { token: self.gateway.settings["token"], key: self.gateway.settings["key"], sandbox: self.gateway.sandbox? }}
+      def state_on_gateway
+        # TODO
       end
 
-      def plan_code
-        "#{self.initiative.permalink[0..29]}#{self.value.to_i}#{'sandbox' if self.gateway.sandbox?}"
-      end
-
-      def plan_name
-        "#{self.initiative.name[0..29]} #{self.value.to_i}#{' (Sandbox)' if self.gateway.sandbox?}"
-      end
-    
-      def customer_code
-        if self.gateway_data && self.gateway_data["customer_code"]
-          self.gateway_data["customer_code"]
-        else
-          Digest::MD5.new.update("#{self.initiative.permalink[0..29]}#{self.user.id}#{'sandbox' if self.gateway.sandbox?}").to_s
-        end
-      end
-      
-      def subscription_code
-        if self.gateway_data && self.gateway_data["subscription_code"]
-          self.gateway_data["subscription_code"]
-        else
-          Digest::MD5.new.update("#{self.initiative.permalink[0..29]}#{self.id}#{'sandbox' if self.gateway.sandbox?}").to_s
-        end
-      end
-
-      def paypal_state_name
-        begin
-          response = Paypal::Assinaturas::Subscription.details(self.subscription_code, self.paypal_auth)
-        rescue
-          return nil
-        end
-        if response && response[:success]
-          status = (response[:subscription]["status"].upcase rescue nil)
-          case status
-            when 'ACTIVE', 'OVERDUE'
-              :active
-            when 'SUSPENDED', 'EXPIRED', 'CANCELED'
-              :suspended
-          end
-        end
-      end
-
-      def update_state_from_gateway!
-        if self.state_name != self.paypal_state_name
-          case self.paypal_state_name
-            when :active
-              self.activate! if self.can_activate?
-            when :suspended
-              self.suspend! if self.can_suspend?
-          end
-        end
+      def update_state_on_gateway!(state)
+        # TODO
       end
 
     end
--- lib/unlock_paypal/models/gateway.rb
+++ lib/unlock_paypal/models/gateway.rb
@@ -9,7 +9,7 @@ module UnlockPaypal
       end
 
       def description
-        "Gerencie mensalidades, assinaturas e cobranças recorrentes com o PayPal"
+        "Economize tempo e dinheiro com as Assinaturas e Cobranças recorrentes do PayPal"
       end
 
       def image
@@ -22,9 +22,10 @@ module UnlockPaypal
 
       def available_settings
         settings = []
-        instructions = "Com sua conta de negócios Paypal habilitada, vá em <a href='https://www.paypal.com.br/AdmMainMenuMyData.do?method=assinaturas' target='_blank'>Ferramentas › Paypal Assinaturas</a> e clique em Habilitar acesso. Depois, vá em <a href='https://www.paypal.com.br/AdmAPI.do?method=keys' target='_blank'>Ferramentas › API Paypal › Chaves de acesso</a>."
-        settings << UnlockGateway::Setting.new(:token, "Token de acesso no Paypal", instructions)
-        settings << UnlockGateway::Setting.new(:key, "Chave de acesso no Paypal", instructions)
+        instructions = "Com sua conta de Especial ou Comercial habilitada no Paypal, vá em <a href='https://www.paypal.com/cgi-bin/customerprofileweb?cmd=_profile-api-access&upgrade.x=1' target='_blank'>Minha conta › Perfil › Mais opções › Minhas ferramentas de venda › Acesso à API › Atualizar</a> e escolha a <strong>Opção 2</strong>. Depois, clique em <a href='https://www.paypal.com/br/cgi-bin/webscr?cmd=_profile-api-signature' target='_blank'>Exibir assinatura de API</a>."
+        settings << UnlockGateway::Setting.new(:username, "Nome do usuário de API", instructions)
+        settings << UnlockGateway::Setting.new(:key, "Senha de API", instructions)
+        settings << UnlockGateway::Setting.new(:key, "Assinatura de API", instructions)
       end
 
     end
